93856ad61ae7e01abf03c6f535f112fcefb6cf71
GabeHasWon <gabriel@guevarameyer.com>
2020-12-22 10:13:13 -0600

    basic content
    wow

---
+++ BigTree.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace BigTree
+{
+	public class BigTree : Mod
+	{
+	}
+}
---
+++ ModGlobalTile.cs
@@ -0,0 +1,26 @@
+﻿using BigTree.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace BigTree
+{
+    class ModGlobalTile : GlobalTile
+    {
+        public override bool CanKillTile(int i, int j, int type, ref bool blockDamaged)
+        {
+            int[] requireGroundTypes = new int[] { TileType<BigTree_Purity>() }; //Makes tiles under big trees unable to be killed
+            if (j > 0 && requireGroundTypes.Any(x => ActiveType(i, j - 1, x)) && SolidTile(i, j)) //Makes sure the position is also in-world
+                return false;
+            return true;
+        }
+
+        public bool ActiveType(int i, int j, int t) => Framing.GetTileSafely(i, j).active() && Framing.GetTileSafely(i, j).type == t; //Some helper methods
+        public bool SolidTile(int i, int j) => Framing.GetTileSafely(i, j).active() && Main.tileSolid[Framing.GetTileSafely(i, j).type];
+    }
+}
